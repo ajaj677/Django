5bb9f7a37c44eca3d444ecc50f18985fa93b8161
ajaj677 <ajajm677@gmail.com>
2025-08-16 00:18:01 +0530

    learn httpsresponse

---
+++ newDjango/newDjango/views.py
@@ -0,0 +1,12 @@
+import re
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("hellow, world. You are at codeZERO")
+    return render(request, 'index.html')
+def about(request):
+    return HttpResponse("hellow, world. You are at codeZERO about page")
+
+def contact(request):
+    return HttpResponse("hellow, world. You are at codeZERO contact page")
